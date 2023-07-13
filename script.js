43096dc7cd2e4de09fbd2c7b461380282c69615c
Ramon Oliveira <ramon.braga.o1997@gmail.com>
2023-07-13 17:41:32 -0400

    create the game function

--- script.js
+++ script.js
@@ -13,25 +13,51 @@ function getComputerChoice() {
 function playRound (computerChoice, playerChoice) {
     computerChoice = getComputerChoice()  //  function should take two parameters, playerSelection and computerSelection
     playerChoice = prompt().toLowerCase()  // make the function playerSelection parameter case-incensitive
-    
-    console.log(computerChoice)
 
 
     if (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors") {
         console.log("you must enter a valid value")
-        return
+        playRound()
     }
     if (playerChoice === computerChoice) {
-        console.log("it's a tie")
-        return
+        return "It's a tie"
     }    //  based on both parameters it should return a string that declares the winner
     if (playerChoice === "paper" && computerChoice === "scissors" || playerChoice === "scissors" && computerChoice === "rock" || playerChoice === "rock" && computerChoice === "paper") {  
         return "You lost this round"
     } else {
-        return "you won this round"
+        return "You won this round"
     }
 }
 // create game function that will use previous functions
-// the game should be a 5 round game
-// game should keep score
-// make game report a winner
+function game() {
+    let i = 0
+    let playerScore = 0  // game should keep score
+    let computerScore = 0
+
+    while (i < 5) {  // the game should be a 5 round game
+        if (playerScore === 3 || computerScore === 3) {
+            break
+        }
+        let result = playRound()
+        if (result === "You lost this round") {
+            computerScore++
+            i++
+        } 
+        if (result === "You won this round") {
+            playerScore++
+            i++
+        }
+        console.log(result)
+        console.log(`You: ${playerScore}   Computer: ${computerScore}`)
+    }
+
+    // make game report a winner
+
+    if (playerScore > computerScore) {
+        console.log("Good job, you beat the machine!")
+    } else {
+        console.log("You lost, try again!")
+    }
+}
+
+game()
