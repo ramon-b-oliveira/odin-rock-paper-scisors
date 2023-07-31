28fe120b1f6ef3f300e843a0ba40d64424105bfe
Ramon Oliveira <ramon.braga.o1997@gmail.com>
2023-07-30 20:38:26 -0400

    introduce ui to the game

--- script.js
+++ script.js
@@ -9,22 +9,56 @@ function getComputerChoice() {
         return "scissors"
     }
 }
+const container = document.querySelector('#winner');
+
+const computerWins = document.createElement('div');
+computerWins.textContent = "Computer wins!";
+const playerWins = document.createElement('div');
+playerWins.textContent = "Player wins!";
+
+
+let playerScoreInt = 0
+let computerScoreInt = 0
+
+
+const playerScore = document.querySelector('#playerScore')
+const computerScore = document.querySelector('#computerScore')
+
+
 // create a function that plays a single round of the game
-function playRound (computerChoice, playerChoice) {
-    computerChoice = getComputerChoice()  //  function should take two parameters, playerSelection and computerSelection
-    playerChoice = prompt().toLowerCase()  // make the function playerSelection parameter case-incensitive
+function playRound (playerChoice) {
+    const computerChoice = getComputerChoice()  //  function should take two parameters, playerSelection and computerSelection
 
+    console.log(computerChoice, playerChoice);
 
-    if (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors") {
-        console.log("you must enter a valid value")
-        playRound()
-    }
     if (playerChoice === computerChoice) {
         return "It's a tie"
     }    //  based on both parameters it should return a string that declares the winner
     if (playerChoice === "paper" && computerChoice === "scissors" || playerChoice === "scissors" && computerChoice === "rock" || playerChoice === "rock" && computerChoice === "paper") {  
-        return "You lost this round"
+        computerScoreInt += 1
+        computerScore.innerHTML = computerScoreInt
+        if (computerScoreInt >= 5) {
+            console.log('computerwin')
+            container.appendChild(computerWins)
+        };
     } else {
-        return "You won this round"
+        playerScoreInt += 1
+        playerScore.innerHTML = playerScoreInt
+        if (playerScoreInt >= 5) {
+            console.log('playerwin')
+            container.appendChild(playerWins)
+        };
     }
 }
+
+const rock = document.querySelector('#rock');
+
+rock.addEventListener('click', () => playRound('rock'));
+
+const paper = document.querySelector('#paper');
+
+paper.addEventListener('click', () => playRound('paper'));
+
+const scissors = document.querySelector('#scissors');
+
+scissors.addEventListener('click', () => playRound('scissors'));
